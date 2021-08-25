030d7a6d1659c524549e696445d4e148849d0b62
Leon Cwiertnia <lcwiertnia@web.de>
2021-08-25 20:46:20 +0200

    Some Changes, SQLite, DBContext, Folder Structure, Renaming

---
+++ Beer Pong/Classes/UserData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beer_Pong.Classes
+{
+    public class UserData
+    {
+        public int UserId { get; set; }
+        public int Wins { get; set; }
+        public int Games { get; set; }
+        public string UserName { get; set; }
+
+        public UserData(string userName, int userId)
+        {
+            UserName = userName;
+            Wins = 0;
+            Games = 0;
+            UserId = userId;
+        }
+    }
+}
---
+++ Beer Pong/DbContextFolder/UserDataDBContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Beer_Pong.Classes;
+
+
+namespace Beer_Pong.DbContextFolder
+{
+    public class UserDataDBContext : DbContext
+    {
+        #region Constructor
+        public UserDataDBContext(DbContextOptions<UserDataDBContext> options)
+            :base(options)
+        {
+
+        }
+        #endregion
+
+        #region Public properties
+        public DbSet<UserData> UserData { get; set; }
+        #endregion
+
+        #region Overriden methods
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserData>().HasData(GetUserData());
+            base.OnModelCreating(modelBuilder);
+        }
+        #endregion
+
+        #region Private methods
+        private List<UserData> GetUserData()
+        {
+            return new List<UserData>
+            {
+
+            };
+        }
+        #endregion
+
+    }
+}
